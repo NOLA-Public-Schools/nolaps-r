fe169f6309313881228ff87a7cf419aec0302edc
sprimeaux-nolapublicschools <sprimeaux@nolapublicschools.com>
2020-09-30 14:10:23 -0500

    Add functions to fetch geographies from SharePoint. Clarify file path functions.

---
+++ R/getdata_geo.R
@@ -0,0 +1,24 @@
+
+
+
+#' @importFrom magrittr %>%
+
+
+
+#' @export
+getdata_zips_orleans <- function() {
+
+  sf::st_read(path_data_clean("Geographies", "zips_orleans.shp"))
+
+}
+
+
+
+#' @export
+getdata_zips_split <- function() {
+
+  sf::st_read(path_data_clean("Geographies", "zips_split.shp"))
+
+}
+
+
--- R/paths.R
+++ R/paths.R
@@ -20,6 +20,24 @@ path_data <- function(x) {
 
 
 #' @export
+path_data_raw <- function(domain, filename) {
+
+  glue::glue(path_data(domain), "/raw/", filename)
+
+}
+
+
+
+#' @export
+path_data_clean <- function(domain, filename) {
+
+  glue::glue(path_data(domain), "/clean/", filename)
+
+}
+
+
+
+#' @export
 path_in <- function(x) {
 
   glue::glue(path_data(x), "/raw/")
