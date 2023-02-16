f33f79110c8f4cae93c70728821a3ae86dd715cb
sprimeaux-nolapublicschools <sprimeaux@nolapublicschools.com>
2023-02-16 12:03:12 -0600

    Comment out overmatch logic for early testing.

--- R/match.R
+++ R/match.R
@@ -174,11 +174,11 @@ match_process <- function(args = commandArgs(trailingOnly = TRUE)) {
     match_augment(appschools = appschools, accounts = accounts, students = students_recent) %>%
     fix_grades()
 
-  overmatches <-
-    readxl::read_excel(
-      glue::glue("{dir_external}/sibling-overmatches.xlsx"),
-      col_types = "text"
-    )
+  # overmatches <-
+  #   readxl::read_excel(
+  #     glue::glue("{dir_external}/sibling-overmatches.xlsx"),
+  #     col_types = "text"
+  #   )
 
   match %>% readr::write_excel_csv(glue::glue("{dir_review}/000_match_to_review.csv"), na = "")
 
