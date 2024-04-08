32d5e8b18bb7a4cd34b70816de49c16a14b5671e
Bowen <asbowen@nolapublicschools.com>
2024-04-08 11:47:40 -0500

    lint and style

--- R/match_augment.R
+++ R/match_augment.R
@@ -1,4 +1,3 @@
-#' @importFrom magrittr %>%
 #' @importFrom glue glue
 #' @importFrom readr read_rds write_rds
 #' @importFrom rlang is_na
@@ -10,32 +9,32 @@
 match_augment <- function(x, gradelevels, contactsmatch) {
 
   names_lookup <-
-    gradelevels %>%
-    select(name_program, choice_school) %>%
+    gradelevels |>
+    select("name_program", "choice_school") |>
     distinct()
 
 
   gradelevels <-
-    gradelevels %>%
+    gradelevels  |>
     select(
-      grade, choice_school, id_gradelevel_guarantee, id_gradecapacity
+      "grade", "choice_school", "id_gradelevel_guarantee", "id_gradecapacity"
     )
 
 
-  x %>%
+  x |>
     left_join(contactsmatch, by = c("STUDENT ID" = "oneappid"),
-             relationship = "many-to-one") %>%
+             relationship = "many-to-one")  |>
     mutate(
       clean_choice_school =
-        if_else(str_detect(`CHOICE SCHOOL`, "Willow|LakeForest"),
-          str_remove(`CHOICE SCHOOL`, "_.*"),
-          `CHOICE SCHOOL`
+        if_else(str_detect(.data$`CHOICE SCHOOL`, "Willow|LakeForest"),
+          str_remove(.data$`CHOICE SCHOOL`, "_.*"),
+          .data$`CHOICE SCHOOL`
         )
-    ) %>%
+    )  |>
     left_join(names_lookup,
       by = c("clean_choice_school" = "choice_school"),
       relationship = "many-to-one"
-    ) %>%
+    )  |>
     left_join(gradelevels,
       by = c(
         "clean_choice_school" = "choice_school",
