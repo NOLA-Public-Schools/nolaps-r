a38dcd2fcc6be4316644dfb59eea3556c9ce77f6
sprimeaux-nolapublicschools <sprimeaux@nolapublicschools.com>
2024-04-08 14:43:45 -0500

    Clean up match_process.

--- R/match.R
+++ R/match.R
@@ -1,59 +1,5 @@
-#' @importFrom readr read_rds write_rds
-
-
-#' @export
-match_lookup_account <- function(x, appschools, accounts) {
-
-  accepted <-
-    x %>%
-    dplyr::filter(stringr::str_length(`STUDENT ID`) == 9) %>%
-    dplyr::distinct(code_appschool = `CHOICE SCHOOL`) %>%
-    dplyr::arrange(code_appschool)
-
-  appschools <-
-    appschools %>%
-    dplyr::select(code_appschool, code_site, id_account) %>%
-    dplyr::filter(complete.cases(.)) %>%
-    dplyr::distinct()
-
-  accounts <-
-    accounts %>%
-    dplyr::select(code_site, id_account) %>%
-    dplyr::filter(complete.cases(.)) %>%
-    dplyr::distinct()
-
-  schools_scholarship <-
-    accepted %>%
-    dplyr::filter(stringr::str_detect(code_appschool, "_[NR]$")) %>%
-    dplyr::mutate(code_site = stringr::str_remove(code_appschool, "_[NR]$")) %>%
-    dplyr::left_join(accounts, by = c("code_site")) %>%
-    dplyr::select(code_appschool, id_account)
-
-  schools_nonscholarship <-
-    accepted %>%
-    dplyr::filter(stringr::str_detect(code_appschool, "_[NR]$", negate = TRUE)) %>%
-    dplyr::left_join(appschools, by = c("code_appschool")) %>%
-    dplyr::select(code_appschool, id_account)
-
-  lookup_account <-
-    dplyr::bind_rows(
-      schools_scholarship,
-      schools_nonscholarship
-    )
-
-  lookup_account
-
-}
-
-
-
-
-
-
-
 #' @export
 match_process <- function(args = commandArgs(trailingOnly = TRUE)) {
-
   dir_in <- str_remove(args[str_detect(args, "--in=")], "--in=")
   dir_out <- str_remove(args[str_detect(args, "--out=")], "--out=")
   round <- str_remove(args[str_detect(args, "--round=")], "--round=")
@@ -71,76 +17,75 @@ match_process <- function(args = commandArgs(trailingOnly = TRUE)) {
 
   dir_review <- glue("{dir_out}/validation")
 
-  if(!dir.exists(dir_business)){dir.create(dir_business)}
-  if(!dir.exists(dir_review)){dir.create(dir_review)}
+  if (!dir.exists(dir_business)) {
+    dir.create(dir_business)
+  }
+  if (!dir.exists(dir_review)) {
+    dir.create(dir_review)
+  }
 
   cat("\nLoading data. Please wait.\n")
 
   if (use_cache) {
+    # accounts <- read_rds(glue("{dir_in}/accounts.rds"))
+    # apps <- read_rds(glue("{dir_in}/apps.rds"))
+    # appinputs <- read_rds(glue("{dir_in}/appinputs.rds"))
+    # appschools <- read_rds(glue("{dir_in}/appschools.rds"))
+    # choices <- read_rds(glue("{dir_in}/choices.rds"))
+    # feeders <- read_rds(glue("{dir_in}/feeders.rds"))
+    # priorities <- read_rds(glue("{dir_in}/priorities.rds"))
+    # siblings <- read_rds(glue("{dir_in}/siblings.rds"))
+    # students_recent <- read_rds(glue("{dir_in}/students_recent.rds"))
 
-    #accounts <- read_rds(glue("{dir_in}/accounts.rds"))
-    #apps <- read_rds(glue("{dir_in}/apps.rds"))
-    #appinputs <- read_rds(glue("{dir_in}/appinputs.rds"))
-    #appschools <- read_rds(glue("{dir_in}/appschools.rds"))
-    #choices <- read_rds(glue("{dir_in}/choices.rds"))
-    #feeders <- read_rds(glue("{dir_in}/feeders.rds"))
-    #priorities <- read_rds(glue("{dir_in}/priorities.rds"))
-    #siblings <- read_rds(glue("{dir_in}/siblings.rds"))
-    #students_recent <- read_rds(glue("{dir_in}/students_recent.rds"))
     gradelevels <- read_rds(glue("{dir_in}/gradelevels.rds"))
-    contactsmatch <- read_rds(glue("{dir_in}/contactsmach.rds"))
-
+    contactsmatch <- read_rds(glue("{dir_in}/contactsmatch.rds"))
   } else {
+    # accounts <- getdata_account()
+    # accounts %>% write_rds(glue("{dir_in}/accounts.rds"))
 
-    #accounts <- getdata_account()
-    #accounts %>% write_rds(glue("{dir_in}/accounts.rds"))
+    # appschools <- getdata_appschool()
+    # appschools %>% write_rds(glue("{dir_in}/appschools.rds"))
 
-    #appschools <- getdata_appschool()
-    #appschools %>% write_rds(glue("{dir_in}/appschools.rds"))
+    # feeders <- getdata_feeder()
+    # feeders %>% write_rds(glue("{dir_in}/feeders.rds"))
 
-    #feeders <- getdata_feeder()
-    #feeders %>% write_rds(glue("{dir_in}/feeders.rds"))
+    # priorities <- getdata_priority()
+    # priorities %>% write_rds(glue("{dir_in}/priorities.rds"))
 
-    #priorities <- getdata_priority()
-    #priorities %>% write_rds(glue("{dir_in}/priorities.rds"))
+    # apps <- getdata_app(round = round)
+    # apps %>% write_rds(glue("{dir_in}/apps.rds"))
 
-    #apps <- getdata_app(round = round)
-    #apps %>% write_rds(glue("{dir_in}/apps.rds"))
+    # appinputs <- getdata_appinput(round = round)
+    # appinputs %>% write_rds(glue("{dir_in}/appinputs.rds"))
 
-    #appinputs <- getdata_appinput(round = round)
-    #appinputs %>% write_rds(glue("{dir_in}/appinputs.rds"))
+    # choices <- getdata_appschoolranking(round = round)
+    # choices %>% write_rds(glue("{dir_in}/choices.rds"))
 
-    #choices <- getdata_appschoolranking(round = round)
-    #choices %>% write_rds(glue("{dir_in}/choices.rds"))
+    # siblings <- getdata_sibling()
+    # siblings %>% write_rds(glue("{dir_in}/siblings.rds"))
 
-    #siblings <- getdata_sibling()
-    #siblings %>% write_rds(glue("{dir_in}/siblings.rds"))
-
-    #students_recent <- getdata_student_recent()
-    #students_recent %>% write_rds(glue("{dir_in}/students_recent.rds"))
-
-    gradelevels <- getdata_gradelevel()
-    gradelevels %>% write_rds(glue("{dir_in}/gradelevels.rds"))
+    # students_recent <- getdata_student_recent()
+    # students_recent %>% write_rds(glue("{dir_in}/students_recent.rds"))
 
     contactsmatch <- getdata_contact_match()
-    contactsmatch %>% write_rds(glue("{dir_in}/contactsmatch.rds"))
+    contactsmatch |> write_rds(glue("{dir_in}/contactsmatch.rds"))
+    contactsmatch |> write_csv(glue("{dir_in}/contactsmatch.csv"), na = "")
 
+    gradelevels <- getdata_gradelevel()
+    gradelevels |> write_rds(glue("{dir_in}/gradelevels.rds"))
+    gradelevels |> write_csv(glue("{dir_in}/gradelevels.csv"), na = "")
   }
 
-  #students_active <- students_recent %>% dplyr::filter(is_active)
+  # students_active <- students_recent %>% dplyr::filter(is_active)
 
   cat("\n")
 
-  # Print the directory path
-  print(glue("Directory in: {dir_in}"))
-
-
   match <-
-    readr::read_csv(
+    read_csv(
       glue("{dir_in}/3_MasterMatch.csv"),
-      col_types = stringr::str_c(stringr::str_dup("c", 9), stringr::str_dup("i", 1), stringr::str_dup("c", 29))
-    ) %>%
-    match_augment(gradelevels = gradelevels, contactsmatch = contactsmatch) %>%
+      col_types = str_c(str_dup("c", 9), str_dup("i", 1), str_dup("c", 29))
+    ) |>
+    match_augment(gradelevels = gradelevels, contactsmatch = contactsmatch) |>
     fix_grades()
 
   # choices_external <-
@@ -155,7 +100,7 @@ match_process <- function(args = commandArgs(trailingOnly = TRUE)) {
   #     col_types = "text"
   #   )
 
-  match %>% readr::write_excel_csv(glue("{dir_review}/000_match_to_review.csv"), na = "")
+  match |> write_csv(glue("{dir_review}/000_match_to_review.csv"), na = "")
 
   return(match)
 
@@ -224,7 +169,4 @@ match_process <- function(args = commandArgs(trailingOnly = TRUE)) {
 
 
   cat(glue("\n\nFinished at {Sys.time()}\n\n"))
-
 }
-
-
